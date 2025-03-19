05a766b462cddad2872f4e0aafe1ec50a79b6db5
Mahmud Nabil <nabilsnigdho@gmail.com>
2025-03-19 20:20:59 +0600

    fix: remove top level await

--- src/features/convert-svg.ts
+++ src/features/convert-svg.ts
@@ -2,13 +2,13 @@ import { ZstdInit, ZstdCodec } from '@oneidentity/zstd-js/wasm'
 import { svg2png } from './svg2png'
 import { Base91 } from '@hpcc-js/wasm-base91'
 
-const { ZstdSimple }: ZstdCodec = await ZstdInit()
-const base91 = await Base91.load()
+const zstd: Promise<ZstdCodec> = ZstdInit()
+const base91 = Base91.load()
 
 export async function convertSVG(svg: string) {
-  const png = await svg2png(svg)
-  const compressedData: Uint8Array = ZstdSimple.compress(png, 20)
-  const encoded_data = base91
+  const png = await (await svg2png)(svg)
+  const compressedData: Uint8Array = (await zstd).ZstdSimple.compress(png, 20)
+  const encoded_data = (await base91)
     .encode(compressedData)
     .replaceAll('<', '-')
     .replaceAll('>', '\\')
--- src/features/svg2png.ts
+++ src/features/svg2png.ts
@@ -1,11 +1,13 @@
 import { createSvg2png, initialize } from 'svg2png-wasm'
 
-await initialize(fetch(`${import.meta.env.BASE_URL}svg2png_wasm_bg.wasm`))
+export const svg2png = (async () => {
+  await initialize(fetch(`${import.meta.env.BASE_URL}svg2png_wasm_bg.wasm`))
 
-const font = await fetch(
-  `${import.meta.env.BASE_URL}fonts/kalpurush.ttf`,
-).then((res) => res.arrayBuffer())
+  const font = await fetch(
+    `${import.meta.env.BASE_URL}fonts/kalpurush.ttf`,
+  ).then((res) => res.arrayBuffer())
 
-export const svg2png = createSvg2png({
-  fonts: [new Uint8Array(font)],
-})
+  return createSvg2png({
+    fonts: [new Uint8Array(font)],
+  })
+})()
